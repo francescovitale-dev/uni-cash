7a7d42ac11e1df01f6caf55a9ac52a7ec8ebc027
francescovitale-dev <francescovitale1307@gmail.com>
2024-04-04 14:26:05 +0200

    .

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Card, Row, Col } from 'react-bootstrap';
-import './landingPage.css'; // Assicurati di importare il tuo file CSS personalizzato
+import { motion } from 'framer-motion';
+import { Fade } from 'react-awesome-reveal';
+import './landingPage.css';
 import trackerPicture from '../../assets/img/trackerPicture.png';
 
 const LandingPage = () => {
@@ -12,12 +14,23 @@ const LandingPage = () => {
             <Card.Body className="text-center">
               <Row className="gx-5 align-items-center justify-content-center justify-content-lg-between">
                 <Col lg={5} className='description-column'>
-                  <h2 className="display-4 lh-1 mb-4">Easy Budget Tracking</h2>
-                  <p className="lead fw-normal text-muted mb-5 mb-lg-0">Whether it's buying alcohol, organizing a party, or enjoying other fun activities, Eurasmus Party Tracker helps you manage your budget efficiently. Say goodbye to budget worries and focus on making memories!</p>
+                  <Fade triggerOnce>
+                    <h2 className="display-4 lh-1 mb-4">Easy Budget Tracking</h2>
+                  </Fade>
+                  <Fade triggerOnce delay={300}>
+                    <p className="lead fw-normal text-muted mb-5 mb-lg-0">Whether it's buying alcohol, organizing a party, or enjoying other fun activities, Eurasmus Party Tracker helps you manage your budget efficiently. Say goodbye to budget worries and focus on making memories!</p>
+                  </Fade>
                 </Col>
                 <Col sm={8} md={6}>
                   <div className="px-5 px-sm-0">
-                    <Card.Img variant="top" src={trackerPicture} alt="tracker picture" className="img-fluid tracker-picture" />
+                    <motion.img 
+                      src={trackerPicture} 
+                      alt="tracker picture" 
+                      className="img-fluid tracker-picture"
+                      initial={{ opacity: 0, y: 20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.5 }}
+                    />
                   </div>
                 </Col>
               </Row>
