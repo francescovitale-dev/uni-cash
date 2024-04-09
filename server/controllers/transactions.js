0928ff1e01be3891c37a8536c1fadab15db1275f
francescovitale-dev <francescovitale1307@gmail.com>
2024-04-09 10:36:26 +0200

    ,

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -3,6 +3,7 @@ const Transaction = require('../models/transactionSchema');
 // Aggiungi una nuova transazione
 const addTransaction = async (req, res) => {
   const { title, amount, category, type } = req.body;
+  const userId = req.user.id; // Recupera l'ID dell'utente dal token JWT
 
   try {
     const transaction = await Transaction.create({
@@ -10,6 +11,7 @@ const addTransaction = async (req, res) => {
       amount,
       category,
       type,
+      userId // Associa l'ID dell'utente alla transazione
     });
 
     res.status(201).json({ success: true, data: transaction });
@@ -18,39 +20,45 @@ const addTransaction = async (req, res) => {
   }
 };
 
+
 // Ottieni tutte le transazioni
-const getTransactions = async (req, res) => {
+const getTransactions= async (req, res) => {
+  const userId = req.user.id; // Recupera l'ID dell'utente dal token JWT
+
   try {
-    const transactions = await Transaction.find();
+    const transactions = await Transaction.find({ userId });
     res.status(200).json({ success: true, data: transactions });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
 };
+
 
 const getTransactionsByType = async (req, res) => {
   const { type } = req.params;
+  const userId = req.user.id; // Recupera l'ID dell'utente dal token JWT
 
   try {
-    const transactions = await Transaction.find({ type });
+    const transactions = await Transaction.find({ type, userId });
     res.status(200).json({ success: true, data: transactions });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
 };
+
 
 // Elimina una transazione
 const deleteTransaction = async (req, res) => {
   const { id } = req.params;
+  const userId = req.user.id; // Recupera l'ID dell'utente dal token JWT
 
   try {
-    const transaction = await Transaction.findById(id);
+    const transaction = await Transaction.findOneAndDelete({ _id: id, userId });
 
     if (!transaction) {
       return res.status(404).json({ success: false, error: 'Transaction not found' });
     }
 
-    await transaction.deleteOne();
     res.status(200).json({ success: true, data: {} });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
